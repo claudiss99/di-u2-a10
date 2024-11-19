a2d256a2125ba00e583adb4960dfb47480f17884
Claudia Perez <claudissperez99@gmail.com>
2024-11-19 13:05:46 +0100

    Haciendo y arreglando ejercicios

--- src/app/Canvas.js
+++ src/app/Canvas.js
@@ -2,20 +2,61 @@ import { useState } from 'react';
 import Background from './Background.js';
 import Box from './Box.js';
 
-const initialPosition = {
+const initialPositionShape = {
   x: 0,
+  y: 0
+};
+
+const initialPositionBackground = {
+  x:0,
   y: 0
 };
 
 export default function Canvas() {
   const [shape, setShape] = useState({
     color: 'orange',
-    position: initialPosition
+    position: initialPositionShape
   });
 
   function handleMove(dx, dy) {
     shape.position.x += dx;
     shape.position.y += dy;
+
+    /**
+     * Otra forma de hacerlo
+     * setShape({
+     *  ...shape,
+     *  position:{
+     *  x: shape.position.x +dx,
+     *  y: shape.position.y +dy
+     * }})
+     * 
+     * Con immer
+     * Habria que importar en vez de useState
+     * import { useImmer} from 'use-immer';
+     * 
+     * const [shape, updateShape] = useImmer({
+      color: 'orange',
+      position: initialPositionShape
+      });
+
+      function handleMove(dx, dy) {
+      updateShape(draft => {
+        draft.position.x += dx
+        draft.position.y += dy
+        })
+      function handleColorChange(e) {
+      updateShape (draft => {
+        draft.color = e.target.value
+        })
+      setShape({
+        ...shape,
+        color: e.target.value
+      });
+  }
+
+
+     */
   }
 
   function handleColorChange(e) {
@@ -36,7 +77,7 @@ export default function Canvas() {
         <option value="aliceblue">aliceblue</option>
       </select>
       <Background
-        position={initialPosition}
+        position={initialPositionBackground}
       />
       <Box
         color={shape.color}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import Canvas from "./Canvas";
+import Scoreboard from "./Scoreboard";
 
 export default function Home() {
   return (
